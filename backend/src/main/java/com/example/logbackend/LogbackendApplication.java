a5214501e56c14b35b18cbfe49d25310dd313761
yejinj <sitting077@gmail.com>
2025-04-30 17:04:36 +0900

    fix: 백엔드 로직 완성

---
+++ backend/src/main/java/com/example/logbackend/LogbackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.logbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.WebApplicationType;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.logbackend")
+public class LogbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(LogbackendApplication.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.run(args);
+    }
+}
